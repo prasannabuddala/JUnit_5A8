e27b99f08ba3639034033fe9d99ad7d5ea3691e2
prasa <prasa@LAPTOP-H0PSVFQQ>
2022-12-04 15:44:50 +0530

    commit 1 mamatha

---
+++ src/main/java/JUnit_5A8/JUnit5A8/App.java
@@ -0,0 +1,16 @@
+package JUnit_5A8.JUnit5A8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public int add(int x,int y) {
+    	return (x+y);
+    }
+}
---
+++ src/test/java/JUnit_5A8/JUnit5A8/AppTest.java
@@ -0,0 +1,23 @@
+package JUnit_5A8.JUnit5A8;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test(){
+    	App a=new App();
+    	int actual=a.add(35,35);
+    	int expected=70;
+    	assertEquals(expected,actual);
+    }
+
+}
